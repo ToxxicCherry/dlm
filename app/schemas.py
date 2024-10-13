24f05e8fc14ba77490067f3041535cd53c40a32f
Pavel kaburneyeu <kps23160@gmail.com>
2024-10-13 14:22:11 +0300

    create pydantic schemas

--- app/schemas.py
+++ app/schemas.py
@@ -1,3 +1,38 @@
 from pydantic import BaseModel
 from typing import Optional
 
+
+class UserBase(BaseModel):
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class User(UserBase):
+    id: int
+    is_admin: bool
+
+    class Config:
+        orm_mode = True
+
+
+class ItemBase(BaseModel):
+    name: str
+    description: Optional[str] = None
+    quantity: int
+
+
+class ItemCreate(ItemBase):
+    pass
+
+
+class Item(ItemBase):
+    id: int
+
+    class Config:
+        orm_mode = True
