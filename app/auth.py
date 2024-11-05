2695cb53f1431118ffb6840074503d83f2f13616
Pavel Kaburneyeu <kps23160@gmail.com>
2024-11-05 19:43:05 +0100

    fix docs auth

--- app/auth.py
+++ app/auth.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 ALGORITHM = 'HS256'
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='/users/token/')
 pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
 
