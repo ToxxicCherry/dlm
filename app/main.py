65d15da60c8442c3bda6f75bef48e3dae7e9a34d
Pavel Kaburneyeu <kps23160@gmail.com>
2024-11-05 19:16:44 +0100

    no more need starlette admin

--- app/main.py
+++ app/main.py
@@ -3,7 +3,6 @@ from app import models
 from app.database import engine
 from app.routers import users_router, items_router, products_router
 from starlette.middleware.sessions import SessionMiddleware
-from starlette_admin.contrib.sqla import Admin, ModelView
 from contextlib import asynccontextmanager
 import os
 from dotenv import load_dotenv
@@ -28,13 +27,6 @@ app.include_router(items_router, prefix="/items", tags=["items"])
 app.include_router(users_router, prefix="/users", tags=["users"])
 app.include_router(products_router, prefix="/products", tags=["products"])
 app.add_middleware(SessionMiddleware, secret_key=SECRET_KEY)
-
-admin = Admin(engine=engine, title='Admin panel')
-admin.add_view(ModelView(models.User))
-admin.add_view(ModelView(models.Item))
-admin.add_view(ModelView(models.Product))
-admin.add_view(ModelView(models.ProductItem))
-admin.mount_to(app)
 
 
 @app.get("/")
